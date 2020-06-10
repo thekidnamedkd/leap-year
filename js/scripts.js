927a7184d559450d7674897c8280b44402559f22
David Cole <davidtcole@yahoo.com>
2020-06-10 10:05:40 -0700

    added further conditions within the leapYear function which will include the next spec in the BDD for this program.
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
+// user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     const year = parseInt($("input#year").val());
     const result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
 
