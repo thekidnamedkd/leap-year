ce0c1c4376d228499368ede5752032b9a0edfaf6
David Cole <davidtcole@yahoo.com>
2020-06-10 09:57:39 -0700

    added further conditions within the leapYear function which will include the next spec in the BDD for this program.
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,9 @@ $(document).ready(function() {
 });
 
 function leapYear(year) {
-  return false;
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
 }
